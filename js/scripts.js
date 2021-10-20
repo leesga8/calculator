00eb8d008cc5f1d7fae15f57067b58da4dbf1f35
Albert Lee <leesga8@gmail.com>
2021-10-20 14:21:16 -0700

    add operator forms and css columns
    co-authored-by: Ella Tanttu <ellajtanttu@gmail.com>
    co-authored-by: Aaron Minnick <abminnick@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,66 @@
-// function add (number1, number2) {
-//   return number1 + number2;
-// }
-
-// function subtract (number1, number2) {
-//   return number1 - number2;
-// }
-// function multiply (number1, number2) {
-//   return number1 * number2;
-// }
-// function divide (number1, number2) {
-//   return number1 / number2;
-// }
-
-function tempConverttoF (number1) {
-  return number1 * (9/5) + 32
-}
-function tempConverttoC (number2) {
-  return number2 -32 * (5/9)
+function add(number1, number2) {
+  return number1 + number2;
 }
 
-const number1 = parseInt(prompt("CHOOSE A TEMPERATURE IN CELSIUS"))
-const number2 = parseInt(prompt("CHOOSE A TEMPERATURE IN FARENH"))
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#sub").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#mult").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mult1").val());
+    const number2 = parseInt($("#mult2").val());
+    const result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#div").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const result = divide(number1, number2);
+    $("#output").text(result);
+  });
+});
 
 
-// const number1 = parseInt(prompt("Enter a number:"));
-// const number2 = parseInt(prompt("Enter another number:"));
-// const result = divide(number1, number2);
 
-const temp1 = tempConverttoF(number1)
-const temp2 = tempConverttoC(number2)
 
-alert("Your frist temp is Celsius:" + temp1 + "and Your second temp is Fareh:" + temp2)
+
+// -----------------------------
+// const number1 = parseInt(prompt("CHOOSE A TEMPERATURE IN CELSIUS"))
+// const number2 = parseInt(prompt("CHOOSE A TEMPERATURE IN FARENH"))
+// function tempConverttoF (number1) {
+//   return number1 * (9/5) + 32
+// }
+// function tempConverttoC (number2) {
+//   return number2 -32 * (5/9)
+// }
+
+
+
+// const temp1 = tempConverttoF(number1)
+// const temp2 = tempConverttoC(number2)
+
+// alert("Your frist temp is Celsius:" + temp1 + "and Your second temp is Fareh:" + temp2)
 
